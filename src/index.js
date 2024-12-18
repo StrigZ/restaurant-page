e7d94194edd3c430c6015608f24292e68d3fbe60
StrigZ <mirgasan200@gmail.com>
2024-12-18 18:41:23 +0300

    Add event listener to nav element to render appropriate section on click

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import "./styles.css";
+import renderStartPage from "./renderStartPage";
+import renderIntensePlan from "./renderIntensePlan";
+import renderStore from "./renderStorePage";
+
+const nav = document.querySelector("nav");
+const main = document.querySelector("main");
+
+(() => {
+  main.append(renderStartPage());
+})();
+
+const renderContentById = (id) => {
+  let content = null;
+  main.innerHTML = "";
+
+  switch (id) {
+    case "start-page-btn":
+      content = renderStartPage();
+      break;
+    case "intense-plan-btn":
+      content = renderIntensePlan();
+      break;
+    case "store-page-btn":
+      content = renderStore();
+      break;
+
+    default:
+      break;
+  }
+
+  main.append(content);
+};
+
+nav.addEventListener("click", ({ target }) => {
+  if (!target.id) {
+    return;
+  }
+
+  renderContentById(target.id);
+});
